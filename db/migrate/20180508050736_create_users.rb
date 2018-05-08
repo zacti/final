8dd81ac87c1d18e13c8800a9e4e0b7ee726aec9a
zacti <ll8317727@gmail.com>
2018-05-08 13:21:03 +0800

    migrate

--- db/migrate/20180508050736_create_users.rb
+++ db/migrate/20180508050736_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-
+      t.string :email
+      t.string :password_digest
+      t.string :auth_token  
+      t.string :password_reset_token
+      t.string :password_reset_sent_at  
       t.timestamps
     end
   end
